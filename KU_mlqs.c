228547b97d2d21eccf5b0aebd7ea5f519bc40994
BonmooKoo <rnqhsan9812@gmail.com>
2022-03-24 13:07:34 +0900

    start

--- KU_mlqs.c
+++ KU_mlqs.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <mqueue.h>
+#include <pthread.h>
 
-int main(){
-    printf("hellow world");
-    printf(":asdf1234");
-    //변경할 부분 있어서 수정하였습니다.
+int main(int args,char* argv[]){
+    const int a1 = atoi(argv[1]);
+	const int a2 = atoi(argv[2]);
+    const int num_thread = atoi(argv[3]);
     
 }
