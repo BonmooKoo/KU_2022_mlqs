eefab46c1b8b42559375a2130811ce16c2d82b53
BonmooKoo <rnqhsan9812@gmail.com>
2022-03-24 22:46:31 +0900

    변수추가

--- KU_mlqs.c
+++ KU_mlqs.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <signal.h>
+#include <time.h>
 #include <unistd.h>
 #include <string.h>
 #include <errno.h>
@@ -7,8 +9,20 @@
 #include <pthread.h>
 
 int main(int args,char* argv[]){
-    const int a1 = atoi(argv[1]);
-	const int a2 = atoi(argv[2]);
-    const int num_thread = atoi(argv[3]);
+    //초기 변수 설정
+    if(args!=3){
+        perror("lack of value");        
+        exit(1);
+    }
+    pnum=atoi(argv[1]);
+    if(pnum>26 || pnum<1){
+        perror("wrong num of processes");
+        exit(1);
+    }
+    const int num_process = atoi(argv[1]);
+	const int ts = atoi(argv[2]);
     
+    //timer 설정
+    
+
 }
